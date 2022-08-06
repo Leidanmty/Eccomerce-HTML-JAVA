211dada384e13cea4c21ddd9038871b5064e69e9
Leidanmty <ld.andrade@hotmail.com>
2022-08-06 15:26:02 -0500

    agregando el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@
 import { Container } from 'react-bootstrap'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { NavBar } from './components'
+import { Home, Login, Product, Purchases } from './pages'
 
 function App() {
   
   return (
     <HashRouter>
+      <NavBar />
       <Container ClassName="mt-5">
         <Routes>
-          <Route path='/' element=""/>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/purchases' element={<Purchases />} />
+          <Route path='/product/:id' element={<Product />} />
         </Routes>
       </Container>
     </HashRouter>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavBar } from 'react-bootstrap';
+
+const NavBar = () => {
+    return (
+        <NavBar>
+            <Container>
+                
+            </Container>
+        </NavBar>
+    );
+};
+
+export default NavBar;
