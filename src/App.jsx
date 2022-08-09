f267633efc629958ab6715be6340248613c2df69
Leidanmty <ld.andrade@hotmail>
2022-08-09 17:59:12 -0500

    Agregando la pantalla de carga

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 import { Container } from 'react-bootstrap'
 import { NavBar } from "./components";
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button, Offcanvas, Form } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
-        <Navbar bg="primary" variant="dark" expand="lg">
-            <Container>
-                <Navbar.Brand href="/#/">E-commerce</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/#/">Log in</Nav.Link>
-                        <Nav.Link href="/#/">Purchases</Nav.Link>
-                        <Nav.Link href="/#/">Cart</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <>
+            <Navbar className="mb-3" bg="success" variant="success" expand='md'>
+                <Container fluid>
+                    <Navbar.Brand href="#">E-commerce</Navbar.Brand>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand`} />
+                    <Navbar.Offcanvas
+                        id={`offcanvasNavbar-expand`}
+                        aria-labelledby={`offcanvasNavbarLabel-expand`}
+                        placement="end"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand`}>
+                                E-commerce
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                                <Nav.Link href="#action1">Home</Nav.Link>
+                                <Nav.Link href="#action2">Link</Nav.Link>
+                                <Nav.Link href="#action3">other</Nav.Link>
+                            </Nav>
+                            <Form className="d-flex">
+                                <Form.Control
+                                    type="search"
+                                    placeholder="Search"
+                                    className="me-2"
+                                    aria-label="Search"
+                                />
+                                <Button variant="outline-dark">Search</Button>
+                            </Form>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
